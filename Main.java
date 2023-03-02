531d6da2d2ec6eaf78d207f54762363d42ae0922
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-03-02 23:30:13 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+package org.example.task2;
+
+import org.example.Product;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Сделать класс Товар абстрактным, создать нескольких наследников (к примеру: БутылкаВоды),
+ * сделать интерфейсом ТорговыйАвтомат и реализовать класс какого-то одного типа ТорговогоАвтомата
+ * (пример: ПродающийБутылкиВодыАвтомат
+ */
+public class Main {
+    public static void main(String[] args) {
+        VendingMachine1 vm = new VendingMachine1();
+        List<BottleOfWater> bw = new ArrayList<>(Arrays.asList(new BottleOfWater("pepsi", 42),
+                new BottleOfWater("sprite", 67),new BottleOfWater("cola", 70),
+                new BottleOfWater("sprite", 40), new BottleOfWater("sprite", 150)));
+        vm.initProduct(bw);
+        System.out.println(vm.getProduct("sprite", 70));
+    }
+}
---
+++ VendingMachine.java
@@ -0,0 +1,7 @@
+package org.example.task2;
+
+import java.util.List;
+
+public abstract class VendingMachine {
+    abstract String getProduct(String name);
+}
